c2fa68bcec3728ee74b3df3d3a281272517e198d
Andrey Kolomoets <kolomoetsanvi@gmail.com>
2019-12-17 23:54:22 +0300

    task3 done

--- 3.php
+++ 3.php
@@ -8,7 +8,8 @@ class newBase
     /**
      * @param string $name
      */
-    function __construct(int $name = 0)
+//    function __construct(int $name = 0)
+    function __construct(string $name = '0')
     {
         if (empty($name)) {
             while (array_search(self::$count, self::$arSetName) != false) {
@@ -41,19 +42,26 @@ class newBase
     public function getSize()
     {
         $size = strlen(serialize($this->value));
-        return strlen($size) + $size;
+//        return strlen($size) + $size;
+        return (string)(strlen($size) + $size);
+
     }
+    //Назнаяение данно фыункции не понятно
+    // возможно имелось ввиду это:
     public function __sleep()
     {
-        return ['value'];
+//        return ['value'];
+        return $this->value;
     }
     /**
      * @return string
      */
     public function getSave(): string
     {
-        $value = serialize($value);
-        return $this->name . ':' . sizeof($value) . ':' . $value;
+//        $value = serialize($value);
+          $value = serialize($this->value);
+//        return $this->name . ':' . sizeof($value) . ':' . $value;
+          return $this->name . ':' . strlen($value) . ':' . $value;
     }
     /**
      * @return newBase
@@ -61,9 +69,17 @@ class newBase
     static public function load(string $value): newBase
     {
         $arValue = explode(':', $value);
-        return (new newBase($arValue[0]))
-            ->setValue(unserialize(substr($value, strlen($arValue[0]) + 1
-                + strlen($arValue[1]) + 1), $arValue[1]));
+//        return (new newBase($arValue[0]))
+//            ->setValue(unserialize(substr($value, strlen($arValue[0]) + 1
+//                + strlen($arValue[1]) + 1), $arValue[1]));
+
+        $obj = new newBase($arValue[0]);
+        $obj->setValue(unserialize(substr($value, strlen($arValue[0]) + 1
+               + strlen($arValue[1]) + 1)));
+        return $obj;
+
+
+
     }
 }
 class newView extends newBase
@@ -89,32 +105,45 @@ class newView extends newBase
     {
         $this->type = gettype($this->value);
     }
+
+    /**
+     *
+     */
     private function setSize()
     {
-        if (is_subclass_of($this->value, "Test3\newView")) {
-            $this->size = parent::getSize() + 1 + strlen($this->property);
-        } elseif ($this->type == 'test') {
-            $this->size = parent::getSize();
-        } else {
-            $this->size = strlen($this->value);
-        }
-    }
+            if (is_subclass_of($this->value, "Test3\newView")) {
+//                $this->size = parent::getSize() + 1 + strlen($this->property);
+                $this->size = parent::getSize() + 1 + strlen((string)$this->property);
+            } elseif ($this->type == 'test') {
+                $this->size = parent::getSize();
+            } else {
+//                $this->size = strlen($this->value);
+                $this->size = strlen((string)$this->value);
+            }
+     }
     /**
      * @return string
      */
+    //Назнаяение данно фыункции не понятно
+    // возможно имелось ввиду это:
     public function __sleep()
     {
-        return ['property'];
+//        return ['property'];
+        return (string)$this->property;
     }
     /**
      * @return string
      */
     public function getName(): string
     {
-        if (empty($this->name)) {
+//        if (empty($this->name)) {
+//            throw new Exception('The object doesn\'t have name');
+//        }
+//        return '"' . $this->name  . '": ';
+        if (empty( parent::getName())) {
             throw new Exception('The object doesn\'t have name');
         }
-        return '"' . $this->name  . '": ';
+        return '"' . parent::getName()  . '": ';
     }
     /**
      * @return string
@@ -147,7 +176,8 @@ class newView extends newBase
     public function getSave(): string
     {
         if ($this->type == 'test') {
-            $this->value = $this->value->getSave();
+//            $this->value = $this->value->getSave();
+            $this->value = parent::getSave();
         }
         return parent::getSave() . serialize($this->property);
     }
@@ -157,15 +187,23 @@ class newView extends newBase
     static public function load(string $value): newBase
     {
         $arValue = explode(':', $value);
-        return (new newBase($arValue[0]))
-            ->setValue(unserialize(substr($value, strlen($arValue[0]) + 1
-                + strlen($arValue[1]) + 1), $arValue[1]))
-            ->setProperty(unserialize(substr($value, strlen($arValue[0]) + 1
-                + strlen($arValue[1]) + 1 + $arValue[1])))
-            ;
+//        return (new newBase($arValue[0]))
+//            ->setValue(unserialize(substr($value, strlen($arValue[0]) + 1
+//                + strlen($arValue[1]) + 1), $arValue[1]))
+//            ->setProperty(unserialize(substr($value, strlen($arValue[0]) + 1
+//                + strlen($arValue[1]) + 1 + $arValue[1])))
+//            ;
+
+        $obj = new newView($arValue[0]);
+        $obj->setValue(unserialize(substr($value, strlen($arValue[0]) + 1
+            + strlen($arValue[1]) + 1)));
+        $obj->setProperty(unserialize(substr($value, strlen($arValue[0]) + 1
+            + strlen($arValue[1]) + 1 + $arValue[1])));
+        return $obj;
     }
 }
-function gettype($value): string
+//function gettype($value): string
+function __gettype($value): string
 {
     if (is_object($value)) {
         $type = get_class($value);
@@ -179,11 +217,14 @@ function gettype($value): string
 }
 
 
+
 $obj = new newBase('12345');
 $obj->setValue('text');
 
 $obj2 = new \Test3\newView('O9876');
-$obj2->setValue($obj);
+//$obj2->setValue($obj);
+$obj2->setValue($obj->__sleep());
+
 $obj2->setProperty('field');
 $obj2->getInfo();
 
@@ -192,4 +233,5 @@ $save = $obj2->getSave();
 $obj3 = newView::load($save);
 
 var_dump($obj2->getSave() == $obj3->getSave());
+
 
